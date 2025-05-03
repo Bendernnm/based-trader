0a3409444824e4b585fa1898bd51394e467037c2
dmytro-wf <dmytro@workfocus.com>
2025-05-03 21:09:42 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { sum } from './utils/sum';
+
+console.log('Hello, TypeScript!');
+console.log('Sum of 1 and 2:', sum(1, 2)); 
---
+++ tests/sum.test.ts
@@ -0,0 +1,9 @@
+import { sum } from '../src/utils/sum';
+
+describe('sum', () => {
+  it('should add two numbers correctly', () => {
+    expect(sum(1, 2)).toBe(3);
+    expect(sum(-1, 1)).toBe(0);
+    expect(sum(0, 0)).toBe(0);
+  });
+}); 
